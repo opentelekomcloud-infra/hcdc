e6788a9cb8d5b8d8f01c5a6e1014a2a187fe0116
Sebastian Gode <sebastian.gode@telekom.de>
2025-04-01 09:37:05 +0000

    added prints

--- src/image_processing.py
+++ src/image_processing.py
@@ -93,8 +93,8 @@ def detect_chars(text, regex_pattern):
         for pattern in regex_pattern:
             char_pattern = re.compile(pattern)
             match = char_pattern.search(text)
-            print(len(match.group(0)))
             if match:
+                print(len(match.group(0)))
                 return {"detected": True, "char": match.group(0)}
             else:
                 return {"detected": False, "char": None}
